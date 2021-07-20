242671d1d007ada63fa5993db55b8a443299e312
Gerald López <gerald.lopez@alumnos.ucn.cl>
2021-07-20 02:19:43 -0400

    fix(login.js): add !

--- routes/handler/user/login.js
+++ routes/handler/user/login.js
@@ -1,28 +1,22 @@
 const { User } = require("../../../models");
 const bcrypt = require("bcryptjs");
-// const { restart } = require("nodemon");
 
 module.exports = async (req, res) => {
     const { email, password } = req.body;
     let user = await User.findOne({
-        where: { email },
+        where: {email},
     });
-
-    if (!user || bcrypt.compareSync(password, user.password)){
-        //condiciotn if user not found or wrong password
-
+    if (!user || !bcrypt.compareSync(password, user.password)) {
         res.status(404).json({
             success: false,
             message: "user not found",
-            data: null,
+            data: null
         });
     }
-
     user.password = undefined;
     res.status(200).json({
         success: true,
         message: "login success",
         data: user,
     });
-
 };
